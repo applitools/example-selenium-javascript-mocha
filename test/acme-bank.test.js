30f37c75c35121c3eab7c91a57792611bc0c5d71
Pandy Knight <andylpk.247@gmail.com>
2023-05-15 19:47:10 -0400

    UFG + Classic + XCloud

--- test/acme-bank.test.js
+++ test/acme-bank.test.js
@@ -2,6 +2,7 @@
 
 const { Builder, By } = require('selenium-webdriver');
 const { Eyes, 
+    ClassicRunner,
     VisualGridRunner, 
     RunnerOptions,
     Target, 
@@ -14,9 +15,15 @@ const { Eyes,
 
 describe('ACME Bank', () => {
     // This Mocha test case class contains everything needed to run a full visual test against the ACME bank site.
-    // It runs the test once locally,
-    // and then it performs cross-browser testing against multiple unique browsers in Applitools Ultrafast Grid.
-
+    // It runs the test once locally.
+    // If you use the Ultrafast Grid, then it performs cross-browser testing against multiple unique browsers.
+    
+    // Settings to control how tests are run.
+    // These could be set by environment variables or other input mechanisms.
+    // They are hard-coded here to keep the example project simple.
+    const USE_ULTRAFAST_GRID = true;
+    const USE_EXECUTION_CLOUD = false;
+    
     // Test control inputs to read once and share for all tests
     var applitoolsApiKey;
     var headless;
@@ -31,10 +38,9 @@ describe('ACME Bank', () => {
     let eyes;
 
     before(async () => {
-        // This method sets up the configuration for running visual tests in the Ultrafast Grid.
+        // This method sets up the configuration for running visual tests.
         // The configuration is shared by all tests in a test suite, so it belongs in a `before` method.
         // If you have more than one test class, then you should abstract this configuration to avoid duplication. 
-
 
         // Read the Applitools API key from an environment variable.
         // To find your Applitools API key:
@@ -46,15 +52,22 @@ describe('ACME Bank', () => {
         // Use headed mode for local development.
         headless = process.env.HEADLESS? ['headless'] : []
 
-        // Create the runner for the Ultrafast Grid.
-        // Concurrency refers to the number of visual checkpoints Applitools will perform in parallel.
-        // Warning: If you have a free account, then concurrency will be limited to 1.
-        runner = new VisualGridRunner(new RunnerOptions().testConcurrency(5));
+        if (USE_ULTRAFAST_GRID) {
+            // Create the runner for the Ultrafast Grid.
+            // Concurrency refers to the number of visual checkpoints Applitools will perform in parallel.
+            // Warning: If you have a free account, then concurrency will be limited to 1.
+            runner = new VisualGridRunner(new RunnerOptions().testConcurrency(5));
+        }
+        else {
+            // Create the classic runner.
+            runner = new ClassicRunner();
+        }
 
         // Create a new batch for tests.
         // A batch is the collection of visual checkpoints for a test suite.
         // Batches are displayed in the Eyes Test Manager, so use meaningful names.
-        batch = new BatchInfo('Example: Selenium JavaScript Mocha with the Ultrafast Grid');
+        const runnerName = (USE_ULTRAFAST_GRID) ? 'Ultrafast Grid' : 'Classic runner';
+        batch = new BatchInfo(`Example: Selenium JavaScript Mocha with the ${runnerName}`);
 
         // Create a configuration for Applitools Eyes.
         config = new Configuration();
@@ -67,16 +80,19 @@ describe('ACME Bank', () => {
         // Set the batch for the config.
         config.setBatch(batch);
 
-        // Add 3 desktop browsers with different viewports for cross-browser testing in the Ultrafast Grid.
-        // Other browsers are also available, like Edge and IE.
-        config.addBrowser(800, 600, BrowserType.CHROME);
-        config.addBrowser(1600, 1200, BrowserType.FIREFOX);
-        config.addBrowser(1024, 768, BrowserType.SAFARI);
-    
-        // Add 2 mobile emulation devices with different orientations for cross-browser testing in the Ultrafast Grid.
-        // Other mobile devices are available, including iOS.
-        config.addDeviceEmulation(DeviceName.Pixel_2, ScreenOrientation.PORTRAIT);
-        config.addDeviceEmulation(DeviceName.Nexus_10, ScreenOrientation.LANDSCAPE);
+        if (USE_ULTRAFAST_GRID) {
+
+            // Add 3 desktop browsers with different viewports for cross-browser testing in the Ultrafast Grid.
+            // Other browsers are also available, like Edge and IE.
+            config.addBrowser(800, 600, BrowserType.CHROME);
+            config.addBrowser(1600, 1200, BrowserType.FIREFOX);
+            config.addBrowser(1024, 768, BrowserType.SAFARI);
+        
+            // Add 2 mobile emulation devices with different orientations for cross-browser testing in the Ultrafast Grid.
+            // Other mobile devices are available, including iOS.
+            config.addDeviceEmulation(DeviceName.Pixel_2, ScreenOrientation.PORTRAIT);
+            config.addDeviceEmulation(DeviceName.Nexus_10, ScreenOrientation.LANDSCAPE);
+        }
     });
     
     beforeEach(async function() {
@@ -85,14 +101,22 @@ describe('ACME Bank', () => {
         // Open the browser with the ChromeDriver instance.
         // Even though this test will run visual checkpoints on different browsers in the Ultrafast Grid,
         // it still needs to run the test one time locally to capture snapshots.
-        driver = new Builder()
-        .withCapabilities({
+        var capabilities = {
             browserName: 'chrome',
             'goog:chromeOptions': {
                 args: headless,
             },
-        })
-        .build()
+        };
+
+        if (USE_EXECUTION_CLOUD) {
+            // Open the browser remotely in the Execution Cloud.
+            let url = await Eyes.getExecutionCloudUrl();
+            driver = new Builder().usingServer(url).withCapabilities(capabilities).build();
+        }
+        else {
+            // Create a local WebDriver.
+            driver = new Builder().withCapabilities(capabilities).build();
+        }
 
         // Set an implicit wait of 10 seconds.
         // For larger projects, use explicit waits for better control.
@@ -103,7 +127,7 @@ describe('ACME Bank', () => {
         // If you are using Selenium 3, use the following call instead:
         // await driver.manage().timeouts().implicitlyWait(10000);
         
-        // Create the Applitools Eyes object connected to the VisualGridRunner and set its configuration.
+        // Create the Applitools Eyes object connected to the runner and set its configuration.
         eyes = new Eyes(runner);
         eyes.setConfiguration(config);
 
